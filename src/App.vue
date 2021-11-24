83335ccd11756544e95bb3106778ce4f02bebf69
Adam Wathan <adam.wathan@gmail.com>
2021-11-24 11:17:12 -0500

    Init

---
+++ src/App.vue
@@ -0,0 +1,16 @@
+<template>
+    <div id="app">
+        <p class="ml-4 text-red-500 mt-4">Test 2</p>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'TailwindTest'
+}
+
+</script>
+
+<style scoped>
+
+</style>
---
+++ tailwind.config.js
@@ -0,0 +1,8 @@
+module.exports = {
+  content: ["./index.html", "./src/app.vue", "./src/index.js"],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
